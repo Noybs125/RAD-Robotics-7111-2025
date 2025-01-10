f762ac6b8e73cf6edbabdb0f1e6f4039ed8df32d
WSPartain <110999745+WSPartain@users.noreply.github.com>
2025-01-09 20:32:16 -0600

    Update Swerve.java

--- Neo-Swerve-2025/src/main/java/frc/robot/subsystems/Swerve.java
+++ Neo-Swerve-2025/src/main/java/frc/robot/subsystems/Swerve.java
@@ -5,10 +5,10 @@ import java.util.function.DoubleSupplier;
 import com.studica.frc.AHRS;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.FollowPathHolonomic;
+import com.pathplanner.lib.commands.FollowPathCommand;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
-import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.geometry.Pose2d;
