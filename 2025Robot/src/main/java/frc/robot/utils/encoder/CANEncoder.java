ecea8279932824d10519fea2fe4114300fbcd071
phillip47 <67516120+phillip47@users.noreply.github.com>
2025-01-18 14:19:46 -0600

    Fully tuned CANEncoder

--- 2025Robot/src/main/java/frc/robot/utils/encoder/CANEncoder.java
+++ 2025Robot/src/main/java/frc/robot/utils/encoder/CANEncoder.java
@@ -6,25 +6,39 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class CANEncoder implements Encoder {
     private final CANcoder encoder;
+    private double gearRatio = 1;
 
     public CANEncoder(int id) {
         encoder = new CANcoder(id);
     }
 
-    public Rotation2d getPos(){
-        return Rotation2d.fromRotations(encoder.getAbsolutePosition().getValueAsDouble());
+    public CANEncoder(int id, double gearRatio) {
+        this.gearRatio = gearRatio;
+        encoder = new CANcoder(id);
     }
 
-    public void setPos(Rotation2d postion){
-        encoder.setPosition(postion);
+    public Rotation2d getPosition(){
+        return Rotation2d.fromRotations(encoder.getAbsolutePosition().getValueAsDouble() * gearRatio);
     }
 
-    public boolean isAbsolute(){}
+    public void setPosition(Rotation2d postion){
+        encoder.setPosition(postion.getRotations());
+    }
 
-    public double getVelocity(){}
+    public boolean isAbsolute(){
+        return true;
+    }
 
-    public double getVoltage(){}
+    public double getVelocity(){
+        return encoder.getVelocity().getValueAsDouble();
+    }
 
-    public void setGearRatio(){}
+    public double getVoltage(){
+        return encoder.getSupplyVoltage().getValueAsDouble();
+    }
+
+    public void setGearRatio(double gearRatio){
+        this.gearRatio = gearRatio;
+    }
     
 }
