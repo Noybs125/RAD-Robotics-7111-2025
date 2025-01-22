c8cdd217a880aa4b77903910dedbd8e9dfc2a532
phillip47 <67516120+phillip47@users.noreply.github.com>
2025-01-21 20:22:55 -0600

    Here

--- 2025Robot/src/main/java/frc/robot/subsystems/Flywheels.java
+++ 2025Robot/src/main/java/frc/robot/subsystems/Flywheels.java
@@ -1,13 +1,17 @@
+package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.motor.Motor;
 
-public class Flywheels extends SubsystemBase
-{
-    public Flywheels(Motor effectorIndexWheels){}
+public class Flywheels extends SubsystemBase {
+
+
+    public Flywheels(Motor effectorIndexWheels){
+
+    }
 
     public void SetSpeed(){}
 
-    public void SetMotorPos(){}
 
     
 
