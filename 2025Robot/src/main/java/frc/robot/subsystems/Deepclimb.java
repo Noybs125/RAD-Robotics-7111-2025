f0b9261aa03bcb5c174fdb0f3620a344b3abc14c
7111RadRobotics <123268447+7111RadRobotics@users.noreply.github.com>
2025-02-03 18:00:46 -0600

    updated deepclimb subsystem

--- 2025Robot/src/main/java/frc/robot/subsystems/Deepclimb.java
+++ 2025Robot/src/main/java/frc/robot/subsystems/Deepclimb.java
@@ -1,7 +1,32 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.motor.Motor;
 
 public class Deepclimb extends SubsystemBase {
     
+    public Motor climbMotor;
+
+    private enum ClimbStates{
+        Climb,
+        Stow,
+    }
+
+    public Deepclimb(Motor climbMotor){
+        this.climbMotor = climbMotor;
+    }
+
+    public void setState(ClimbStates state){
+        switch (state) {
+            case Climb:
+                climbMotor.setSpeed(.5);
+                break;
+            
+            case Stow:
+                climbMotor.setSpeed(-.5);
+                break;
+            default:
+                break;
+        }
+    }
 }
