6976348ce4e025b652710208bab9e49f826bc8cf
WSPartain <110999745+WSPartain@users.noreply.github.com>
2025-02-22 14:13:36 -0600

    SwerveModule javadocs added fully

--- 2025Robot/src/main/java/frc/robot/utils/SwerveModule.java
+++ 2025Robot/src/main/java/frc/robot/utils/SwerveModule.java
@@ -39,7 +39,15 @@ public class SwerveModule {
   private final double canCoderOffsetRotations;
   private final CANcoderConfigurator canConfig;
 
-  // Constructor
+  /**
+   * Constructor for SwerveModule class.
+   * Creates drive motor object.
+   * Creates angle motor object.
+   * creats feedforward object for the motor.
+   * Creates CANCoder object.
+   * @param moduleNumber -Type "int", used to specify module number properties for the objects.
+   * @param constants -Type "SwerveModuleConstants", used to allow the objects to access the motor constants.
+   */
   public SwerveModule(int moduleNumber, SwerveModuleConstants constants) {
     this.moduleNumber = moduleNumber;
     
@@ -55,7 +63,13 @@ public class SwerveModule {
 
     configureDevices();
   }
-
+  /**
+   * Sets the state of the swerve module. 
+   * Uses the SwerveModuleState objects.
+   * @param state -Type "SwerveModuleState", it is an object defining what state the module is in.
+   * @param isOpenLoop -Type "boolean", true if is open loop, false if otherwise.
+   * @see https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/kinematics/SwerveModuleState.html.
+   */
   public void setState(SwerveModuleState state, boolean isOpenLoop) {
     // Prevents angle motor from turning further than it needs to. 
     // E.G. rotating from 10 to 270 degrees CW vs CCW.
@@ -79,41 +93,95 @@ public class SwerveModule {
 
   }
 
+  /**
+   * Constructs a new SwerveModuleState object.
+   * Uses getVelocity, fromRotations, and SwerveModuleState methods.
+   * @return -Type "SwerveModuleState", SwerveModuleState object created based off of velocity and rotation
+   * @see -Link to getVelocity: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreTalonFX.html#getVelocity().
+   * @see -Link to fromRotations: https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/geometry/Rotation2d.html#fromRotations(double).
+   * @see -Link to SwerveModuleState: https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/kinematics/SwerveModuleState.html#%3Cinit%3E(double,edu.wpi.first.math.geometry.Rotation2d).
+   */
   public SwerveModuleState getState() {
     double velocity = driveMotor.getVelocity().getValueAsDouble() * 60 * Constants.kSwerve.DRIVE_RPM_TO_METERS_PER_SECOND;
     Rotation2d rot = Rotation2d.fromRotations(angleMotor.getPosition().getValueAsDouble());
     return new SwerveModuleState(velocity, rot);
   }
 
+  /**
+   * Constructs a new SwerveModuleState object, but inverts the velocity, inverting the state.
+   * Uses getVelocity, fromRotations, and SwerveModuleState methods.
+   * @return -Type "SwerveModuleState", SwerveModuleState object created based off of velocity and rotation
+   * @see -Link to getVelocity: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreTalonFX.html#getVelocity().
+   * @see -Link to fromRotations: https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/geometry/Rotation2d.html#fromRotations(double).
+   * @see -Link to SwerveModuleState: https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/kinematics/SwerveModuleState.html#%3Cinit%3E(double,edu.wpi.first.math.geometry.Rotation2d).
+   */
   public SwerveModuleState getInvertedState() {
     double velocity = driveMotor.getVelocity().getValueAsDouble() * 60 * Constants.kSwerve.DRIVE_RPM_TO_METERS_PER_SECOND;
     Rotation2d rot = Rotation2d.fromRotations(angleMotor.getPosition().getValueAsDouble());
     return new SwerveModuleState(-velocity, rot);
   }
 
+  /**
+   * Uses getAbsolutePosition method to get the position of the canCoder.
+   * @return -Type "double", gets the canCoder absolute position.
+   * @see -Link to getAbsolutePosition method: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreCANcoder.html#getAbsolutePosition().
+   */
   public double getCanCoder() {
     return canCoder.getAbsolutePosition().getValueAsDouble();
   }
 
+  /**
+   * gets the angle of the motor from the motor encoder.
+   * @return Type "Rotation2d", gives a rotation2d object containing the motor rotation as an angle. Unit is rotation.
+   * @see -Link to getPosition: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreTalonFX.html#getPosition().
+   */
   public Rotation2d getAngle() {
     return Rotation2d.fromRotations(angleMotor.getPosition().getValueAsDouble());
   }
 
+  /**
+   * Uses getPosition and fromRotations to find distance and rotation to supply SwerveModulePosition.
+   * @return -Type "SwerveModulePosition", SwerveModulePosition object based off of the distance of the motor and the rotation
+   * @see -Link to getPosition: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreTalonFX.html#getPosition()
+   * @see -Link to fromRotations: https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/geometry/Rotation2d.html#fromRotations(double).
+   * @see -Link to SwerveModulePosition: https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/kinematics/SwerveModulePosition.html#%3Cinit%3E(double,edu.wpi.first.math.geometry.Rotation2d).
+   */
   public SwerveModulePosition getPosition() {
     double distance = driveMotor.getPosition().getValueAsDouble() * Constants.kSwerve.WHEEL_CIRCUMFERENCE;
     Rotation2d rot = Rotation2d.fromRotations(angleMotor.getPosition().getValueAsDouble());
     return new SwerveModulePosition(distance, rot);
   }
+
+  /**
+   * Uses getPosition and fromRotations to find distance and rotation to supply SwerveModulePosition. Inverts rotation.
+   * @return -Type "SwerveModulePosition", SwerveModulePosition object based off of the distance of the motor and the rotation
+   * @see -Link to getPosition: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreTalonFX.html#getPosition()
+   * @see -Link to fromRotations: https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/geometry/Rotation2d.html#fromRotations(double).
+   * @see -Link to SwerveModulePosition: https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/math/kinematics/SwerveModulePosition.html#%3Cinit%3E(double,edu.wpi.first.math.geometry.Rotation2d).
+   */
   public SwerveModulePosition getInvertedPosition() {
     double distance = driveMotor.getPosition().getValueAsDouble() * Constants.kSwerve.WHEEL_CIRCUMFERENCE;
     Rotation2d rot = Rotation2d.fromRotations(angleMotor.getPosition().getValueAsDouble());
     return new SwerveModulePosition(distance, rot.unaryMinus());
   }
 
+  /**
+   * Gets the angle of the CanCoder using absolute position
+   * Uses getAbsolutePosition method.
+   * @return -Type "Rotation2d", assignes the rotation value to a rotation 2d object as properties.
+   * @see -Link to getAbsolutePosition: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreCANcoder.html#getAbsolutePosition().
+   */
   public Rotation2d getCanCoderDegrees(){
     return Rotation2d.fromDegrees(canCoder.getAbsolutePosition().getValueAsDouble() * 360);
   }
   
+  /**
+   * Configures all of the devices in this class, including the constants.
+   * Uses setPosition and getConfigurator methods, along with the CANcoderConfiguration class.
+   * @see -Link to setPosition: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreTalonFX.html#setPosition(edu.wpi.first.units.measure.Angle).
+   * @see -Link to getConfigurator: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreTalonFX.html#getConfigurator().
+   * @see -Link to CANcoderConfiguration: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/configs/CANcoderConfiguration.html.
+   */
   private void configureDevices() {
     // CanCoder configuration.
     CANcoderConfiguration canCoderConfiguration = new CANcoderConfiguration();
@@ -122,11 +190,9 @@ public class SwerveModule {
     
     canConfig.apply(canCoderConfiguration);
   
+
+
     // Drive motor configuration.
-
-
-    
-
     driveMotorConfig.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = Constants.kSwerve.OPEN_LOOP_RAMP;
     driveMotorConfig.ClosedLoopRamps.DutyCycleClosedLoopRampPeriod = Constants.kSwerve.CLOSED_LOOP_RAMP;
     
