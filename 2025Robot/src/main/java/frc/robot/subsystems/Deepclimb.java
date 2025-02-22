ace979c436e425f72641bf8bc3a500a62787152e
WSPartain <110999745+WSPartain@users.noreply.github.com>
2025-02-21 18:09:16 -0600

    Added javadocs to deepclimb and flywheel

--- 2025Robot/src/main/java/frc/robot/subsystems/Deepclimb.java
+++ 2025Robot/src/main/java/frc/robot/subsystems/Deepclimb.java
@@ -7,15 +7,26 @@ public class Deepclimb extends SubsystemBase {
     
     public Motor climbMotor;
 
+    /**
+     * State of robot climb winch, detailing either in the high state "Climb", and the stowwed state "Stow".
+     */
     private enum ClimbStates{
         Climb,
         Stow,
     }
 
+    /**
+     * Initilizes the motor and constructor to class "Deepclimb".
+     * @param climbMotor -Motor that operates winch for the Deepclimb mechanism.
+     */
     public Deepclimb(Motor climbMotor){
         this.climbMotor = climbMotor;
     }
 
+    /**
+     * Sets state of Deepclimb, either being stowed or climbing.
+     * @param state -Type "ClimbStates", either raises or loweres winch for climb.
+     */
     public void setState(ClimbStates state){
         switch (state) {
             case Climb:
--- 2025Robot/src/main/java/frc/robot/subsystems/Flywheels.java
+++ 2025Robot/src/main/java/frc/robot/subsystems/Flywheels.java
@@ -8,26 +8,53 @@ public class Flywheels extends SubsystemBase {
 
     public Motor effectorWheels;
     
+    /**
+     * State enum for the flywheels, states include "CoralIntake", "CoralOutake", "AlgaeIntake", "AlgaeOutake".
+     * These states activate what motors on the end effector do what.
+     */
     private enum WheelsStates{
         CoralIntake,
-        AlgaeIntake,
         CoralOutake,
+        AlgaeIntake,
         AlgaeOutake
     };
 
+    /**
+     * Constructor for class "Flywheels". Initilizes the end effector motor.
+     */
     public Flywheels(){
          effectorWheels = new REVMotor(0);
     }
 
+    /**
+     * Sets the speed of the end effector using the .set method in TalonFX.
+     * Review the following link for more information (for CTRE motors):
+     * {@link https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/TalonFX.html#set(double)}
+     * @param speed -Type "Double", used to set the speed of the motor for the effector wheels. valid values are between -1.0 and 1.0.
+     */
     public void setSpeed(double speed){
         effectorWheels.setSpeed(speed);
     }
 
+    /**
+     * Returns the speed of the motor as a double.
+     * Review the following link for more information (for CTRE motors):
+     * {@link https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/TalonFX.html#get()}
+     * @return -The speed of the motors, as a double between -1.0 and 1.0.
+     */    
     public double getSpeed(){
         return effectorWheels.getSpeed();
     }
     
-    public void setSpeed(WheelsStates speed){
+    /**
+     * Sets the state of the end effector motor, using the .set(double) method.
+     * Unlike the method "setSpeed", uses states rather than a direct double.
+     * States include; CoralIntake, CoralOutake, AlgaeIntake, AlgaeOutake.
+     * Review the following link for more information on the used method (for CTRE motors):
+     * {@link https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/TalonFX.html#set(double)}
+     * @param speed -Type "WheelsStates", states for taking or dropping algae or coral. Valid values are between -1.0 and 1.0.
+     */
+    public void setSpeedState(WheelsStates speed){
         switch (speed) {
             case CoralIntake:
                 effectorWheels.setSpeed(.5);
