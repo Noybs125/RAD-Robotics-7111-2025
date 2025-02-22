f7322d41ca4be961ba1717802b20d45fc0cd3570
Noybs125 <129349581+Noybs125@users.noreply.github.com>
2025-02-22 10:55:10 -0600

    added generating autonomous routines its very messy though it needs cleaning up and isn't finished

--- 2025Robot/src/main/java/frc/robot/utils/AutoCycle.java
+++ 2025Robot/src/main/java/frc/robot/utils/AutoCycle.java
@@ -1,22 +1,21 @@
 package frc.robot.utils;
 
-import edu.wpi.first.wpilibj2.command.Command;
 
 /**
- * This class handles a cycle in a custom auto routine.⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ * This class represents a cycle in a custom auto routine.⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
  * It handles when the routine executes, which feeder station to intake from, the reef face, level, and branch to score on
  */
 public class AutoCycle {
 
-    private final int executedPosition;
-    private final int feederStation;
-    private final int reefFace;
-    private final int levelToScore;
-    private final boolean isLeft;
+    public final int executedPosition;
+    public final int feederStation;
+    public final int reefFace;
+    public final int levelToScore;
+    public final boolean isLeft;
 
     /**
      * Initializes the Auto cycle
-     * @param executedPosition The place in the sequential command group the command executes
+     * @param executedPosition The position in the sequential command group the command executes
      * @param feederStation The feeder station to intake the gamepiece from, 1 = left, 2 = right, 0 = start config
      * @param reefFace The side of the reef (1 - 6) to score the gamepiece
      * @param levelToScore The reef level (1-4) to score the gamepiece on
@@ -28,13 +27,5 @@ public class AutoCycle {
         this.isLeft = isLeft;
         this.levelToScore = levelToScore;
         this.reefFace = reefFace;
-    }
-
-    public int getExecutedPosition(){
-        return executedPosition;
-    }
-
-    public Command getCommand(){
-        return null;
     }
 }
