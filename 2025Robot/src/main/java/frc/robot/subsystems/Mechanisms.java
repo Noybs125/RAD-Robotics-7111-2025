ab7ac952644e745458b36f488b646116011a26de
WSPartain <110999745+WSPartain@users.noreply.github.com>
2025-02-21 20:30:01 -0600

    mechanisms javadoc comments added

--- 2025Robot/src/main/java/frc/robot/subsystems/Mechanisms.java
+++ 2025Robot/src/main/java/frc/robot/subsystems/Mechanisms.java
@@ -31,6 +31,10 @@ public class Mechanisms extends SubsystemBase {
     private double upperLimit;
     private MechanismsState state = MechanismsState.Store;
 
+    /**
+     * States for choosing what position to set mechanisms to.
+     * States include: "ReefL1" through "ReefL4", "AlgaeL2" and "AlgaeL3", "AlgaeProcessor", "AlgaeNet", "Store", "CoralFeeder", and "Climb".
+     */
     public enum MechanismsState{
         ReefL1,
         ReefL2,
@@ -53,6 +57,13 @@ public class Mechanisms extends SubsystemBase {
     private MechanismLigament2d elevatorMech2d = elevatorMech2dRoot.append(new MechanismLigament2d("elevator", elevatorMinimumLength, 90));
     private MechanismLigament2d wristMech2d = wristMech2dRoot.append(new MechanismLigament2d("wrist", wristLength, 0));
 
+    /**
+     * Constructor for Mechanisms Class.
+     * Initilizes testEncoder, elevator, and wrist objects using WpiEncoder, ElevatorSimMotor, and ArmSimMotor respectively.
+     * @see -WpiEncoder class is located under frc.robot.utils.encoder.WpiEncoder.
+     * @see -ElevatorSimMotor class is located under frc.robot.utils.motor.ElevatorSimMotor.
+     * @see -ArmSimMotor class is located under frc.robot.utils.motor.ArmSimMotor.
+     */
     public Mechanisms(){
         testEncoder = new WpiEncoder(0, 1);
         elevator = new ElevatorSimMotor(
@@ -62,51 +73,104 @@ public class Mechanisms extends SubsystemBase {
         wrist =  new ArmSimMotor(null, Constants.kSimulation.armSim, Constants.kSimulation.wristPid,  null);//Constants.kSimulation.wristFF));
     }
     
+    /**
+     * Sets wristSetpoint equal to setPoint input and sets the isManual boolean to false.
+     * WristSetpoint is the variable that controls where the wrist to the end effector is going to try to go.
+     * @see -Affects elevator setpoint
+     * @see -Inputs for "moveElevThenArm" and "moveArmThenElev"
+     * @param setPoint -Type "double", used to set the setpoint for the wrist, and eventually move the wrist to this variable. UNIT UNKNOWN.
+     */
     public void setWristSetpoint(double setPoint) {
         wristSetpoint = setPoint;
         isManual = false;
     }
 
+    /**
+     * Sets elevatorSetPoint equal to setPoint input and sets the isManual boolean to false.
+     * ElevatorSetPoint is the variable that controls where the elevator is trying to go to
+     * @see -Affects elevator setpoint
+     * @see -Inputs for "moveElevThenArm" and "moveArmThenElev"
+     * @param setPoint -Type "double", used to set the setpoint for the wrist, and eventually move the wrist to this variable.TODO: SetPoint UNIT UNKNOWN.
+     */
     public void setElevatorSetpoint(double setPoint) {
         elevatorSetpoint = setPoint;
         isManual = false;
     }
     /**
-     * This method uses circumference of elevator winch and relative position of encoder to calculate elevator height.
-     * @return calculated elevator height in feet. TODO: Change method return to return percent of max height to give more predictable units 
+     * Uses circumference of elevator winch and relative position of encoder to calculate elevator height. Uses getPosition method.
+     * @return Calculated elevator height in feet. TODO: Change method return to return percent of max height to give more predictable units.
+     * @see -Link to getPosition: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/TalonFX.html#get().
      */
     public double getElevatorHeight(){
         double elevatorHeight = (Math.PI * kMechanisms.elevatorWinchDiameter * elevator.getPosition() / 360.0) * 12;
         return elevatorHeight;
     }
 
+    /**
+     * Sets the elevator motor speed, raising or lowering the elevator.
+     * (If using CTRE) Uses set method.
+     * @param speed -Type "double", variable limits between -1.0 and 1.0. -1.0 is full speed down, and 1.0 is full speed up. TODO: make sure -1 is down and 1 is up.
+     * @see -Link to set(double) method: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/TalonFX.html#set(double).
+     */
     public void setElevatorSpeed(double speed) {
         elevator.setSpeed(speed);
         isManual = true;
     }
 
+    /**
+     * Sets the end effector's wrist speed, turning it clockwise or counterclockwise.
+     * (If using CTRE) Uses set method
+     * @param speed -Type "double", variable limits between -1.0 and 1.0. -1.0 is full speed down, and 1.0 is full speed up. TODO: make sure -1 is down and 1 is up.
+     * @see -Link to set(double) method: https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/TalonFX.html#set(double).
+     */
     public void setWristSpeed(double speed) {
         wrist.setSpeed(speed);
         isManual = true;
     }
 
+    /**
+     * Sets both elevator setpoint and wrist setpoint, which directly states where the elevator and wrist should go to respectively.
+     * @see -Affects "moveElevThenArm" inputs, and "moveArmThenElev" inputs.
+     * @param wristSetpoint -Type "double", Sets the wristSetPoint variable, which directly controls where the wrist should try to go to.TODO: wristSetpoint UNIT UNKNOWN.
+     * @param elevatorSetpoint -Type "double", Sets the elevatorSetpoint variable, which directly controls where the elevator should try to go to.TODO: elevatorSetpoint UNIT UNKNOWN.
+     */
     public void setAllMechanismsSetpoint(double wristSetpoint, double elevatorSetpoint) {
         this.elevatorSetpoint = elevatorSetpoint;
         this.wristSetpoint = wristSetpoint;
         isManual = false;
     }
 
+    /**
+     * Sets the state for mechanisms, deciding what handleState does.
+     * States includes: "Reef1" through "Reef4", "AlgaeL2" and "AlgaeL3", "AlgaeProcessor", "AlgaeNet", "Store", "CoralFeeder" and "Climb".
+     * @param state -Type "MechanismsState", controls the state that the "handleState" method is in. 
+     */
     public void setState(MechanismsState state){
         this.state = state;
     }
 
+    /**
+     * Updates the class's elevator setpoint variable and waits for the elevator to reach position before updating the arm setpoint.
+     * Uses isAtSetpoint method.
+     * @param elevatorSetpoint -Type "double", current elevator setpoint. Used to update the class's elevator setpoint.
+     * @param wristSetpoint -Type "double", current wrist setpoint. Used to update the class's elevator setpoint.
+     * @param deadzone -Type "double", amount of error allowed to determine if the elevator is in position.
+     * @see -isAtSetpoint method is located under: frc.robot.utils.motor.CTREMotor.
+     */
     public void moveElevThenArm(double elevatorSetpoint, double wristSetpoint, double deadzone){
         this.elevatorSetpoint = elevatorSetpoint;
         if(elevator.isAtSetpoint(deadzone)){
             this.wristSetpoint = wristSetpoint;
         }
     }
-
+    /**
+     * Updates the class's arm setpoint variable and waits for the arm to reach position before updating the elevator setpoint.
+     * Uses isAtSetpoint method.
+     * @param elevatorSetpoint -Type "double", current elevator setpoint. Used to update the class's elevator setpoint.
+     * @param wristSetpoint -Type "double", current wrist setpoint. Used to update the class's elevator setpoint.
+     * @param deadzone -Type "double", amount of error allowed to determine if the wrist is in position.
+     * @see -isAtSetpoint method is located under: frc.robot.utils.motor.CTREMotor.
+     */
     public void moveArmThenElev(double elevatorSetpoint, double wristSetpoint, double deadzone){
         this.wristSetpoint = wristSetpoint;
         if(wrist.isAtSetpoint(deadzone)){
@@ -114,6 +178,10 @@ public class Mechanisms extends SubsystemBase {
         }
     } 
 
+    /**
+     * Sets the elevator setpoint and the wrist setpoint based on the current mechanisms state.
+     * States include: "ReefL1" through "ReefL4", "AlgaeL2" and "AlgaeL3", "AlgaeProcessor", "AlgaeNet", "Store", "CoralFeeder", and "Climb".
+     */
     private void handleState() {
         switch (state) {
             case ReefL1:
